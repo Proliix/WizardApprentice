1e8ab00bb46b4b7782af5fc896503281eb7fd9a4
Proliix <lucas.andreasson@hotmail.com>
2022-11-23 11:19:02 +0100

    Added trippleShot Card

--- Wizard Apprentice/Assets/Scripts/Cards/TrippleShotCard.cs
+++ Wizard Apprentice/Assets/Scripts/Cards/TrippleShotCard.cs
@@ -1,13 +1,14 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NormalCard : MonoBehaviour, ICard
+public class TrippleShotCard : MonoBehaviour,ICard
 {
+
     [SerializeField] Sprite image;
     [SerializeField] Transform Spawnpoint;
     [SerializeField] float shootCooldown = 0.25f;
+    [SerializeField] float shootPosDeviation = 0.25f;
 
     BulletHandler bulletHandler;
     GameObject player;
@@ -18,10 +19,11 @@ public class NormalCard : MonoBehaviour, ICard
         bulletHandler = GameObject.FindWithTag("GameController").GetComponent<BulletHandler>();
         player = GameObject.FindWithTag("Player");
     }
-
     public void Effect()
     {
-        bulletHandler.GetBullet(Spawnpoint.position, player, true,false);
+        bulletHandler.GetBullet(Spawnpoint.position, player, true, false);
+        bulletHandler.GetBullet(Spawnpoint.position + (Vector3.right * shootPosDeviation), player, true, true);
+        bulletHandler.GetBullet(Spawnpoint.position - (Vector3.right * shootPosDeviation), player, true, true);
     }
 
     public Sprite GetSprite()
@@ -38,11 +40,11 @@ public class NormalCard : MonoBehaviour, ICard
     {
         timer += Time.deltaTime;
 
-        if(timer >= shootCooldown)
+        if (timer >= shootCooldown)
         {
             timer = 0;
             Effect();
         }
-
     }
+
 }
