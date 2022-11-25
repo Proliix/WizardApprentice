82e54b6113f3efd3c8367f8c719fe27a7368966f
Proliix <lucas.andreasson@hotmail.com>
2022-11-25 14:21:13 +0100

    Made icard public

--- Wizard Apprentice/Assets/Scripts/Cards/ICard.cs
+++ Wizard Apprentice/Assets/Scripts/Cards/ICard.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 
-interface ICard
+public interface ICard
 {
+
+    //gets called when a card returns to its normal state
     public void ResetCard();
-
+    //the cards effect
     public void Effect();
-
+    //use this as the update function, it gets called when a card is active
     public void UpdateCard();
-
+    //Return the sprite of the card
     public Sprite GetSprite();
 }
