596372297fa70ee0fd4bf6cf7fdb7b9bf2de1a10
Proliix <lucas.andreasson@hotmail.com>
2022-12-06 09:07:47 +0100

    Book companion code cleanup

--- Wizard Apprentice/Assets/Scripts/BookCompanion.cs
+++ Wizard Apprentice/Assets/Scripts/BookCompanion.cs
@@ -6,24 +6,18 @@ public class BookCompanion : MonoBehaviour
 {
     [SerializeField] Sprite[] sprites;
 
-    float[] angles;
-    float currentAngle;
-    float targetAngle;
-    float deg;
-    float halfDeg;
+    int playerSorting;
     GameObject shootPos;
     SpriteRenderer sr;
-    int index;
     PlayerAiming playerAiming;
 
     // Start is called before the first frame update
     void Start()
     {
-        shootPos = GameObject.FindWithTag("Player").GetComponent<PlayerAiming>().bulletSpawn;
         playerAiming = GameObject.FindWithTag("Player").GetComponent<PlayerAiming>();
+        shootPos = playerAiming.bulletSpawn;
+        playerSorting = playerAiming.gameObject.GetComponent<SpriteRenderer>().sortingOrder;
         sr = gameObject.GetComponent<SpriteRenderer>();
-        deg = 360 / sprites.Length;
-        halfDeg = deg / 2;
     }
 
     // Update is called once per frame
@@ -32,11 +26,11 @@ public class BookCompanion : MonoBehaviour
 
         Vector2 dir = playerAiming.dir.normalized;
         sr.sprite = sprites[((Mathf.FloorToInt((((Mathf.Atan2(dir.y, dir.x) + Mathf.PI) / (Mathf.PI * 2)) + 0.5f / sprites.Length) * sprites.Length)) + Mathf.FloorToInt(sprites.Length / 4)) % sprites.Length];
-       
+
         if (dir.y < 0)
-            sr.sortingOrder = 11;
+            sr.sortingOrder = playerSorting + 1;
         else
-            sr.sortingOrder = 9;
+            sr.sortingOrder = playerSorting - 1;
 
         transform.position = shootPos.transform.position;
     }
