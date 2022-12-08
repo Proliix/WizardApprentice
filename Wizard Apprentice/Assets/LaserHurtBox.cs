be4b62e8e3a365a975ee27c10cc291fa1877dff1
Proliix <lucas.andreasson@hotmail.com>
2022-12-08 14:41:17 +0100

    Update LaserHurtBox.cs

--- Wizard Apprentice/Assets/LaserHurtBox.cs
+++ Wizard Apprentice/Assets/LaserHurtBox.cs
@@ -8,7 +8,12 @@ public class LaserHurtBox : MonoBehaviour
     [SerializeField] float attackDelay;
     [SerializeField] float timer;
 
+    List<Health> enemyhealth;
 
+    private void Start()
+    {
+        enemyhealth = new List<Health>();
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -17,15 +22,46 @@ public class LaserHurtBox : MonoBehaviour
 
             if (collision.gameObject.CompareTag("Enemy"))
             {
-                timer -= attackDelay;
                 collision.gameObject.GetComponent<Health>().RemoveHealth(damage);
             }
         }
 
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (collision.gameObject.GetComponent<Health>() != null)
+                enemyhealth.Add(collision.gameObject.GetComponent<Health>());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (collision.gameObject.GetComponent<Health>() != null)
+                enemyhealth.Remove(collision.gameObject.GetComponent<Health>());
+        }
+    }
+
     private void Update()
     {
         timer += Time.deltaTime;
+        if (timer >= attackDelay)
+        {
+            timer = 0;
+
+            for (int i = 0; i < enemyhealth.Count; i++)
+            {
+                if (enemyhealth[i] != null)
+                {
+                    enemyhealth[i].RemoveHealth(damage);
+                }
+            }
+
+        }
+
     }
 }
