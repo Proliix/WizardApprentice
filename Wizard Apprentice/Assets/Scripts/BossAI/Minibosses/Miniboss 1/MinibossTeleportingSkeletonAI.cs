325ee4ffa0c5730be13eb0e49d2f644bc7a43943
sandeeen <60133440+sandeeen@users.noreply.github.com>
2022-12-08 13:46:38 +0100

    Tutorial room and miniboss

--- Wizard Apprentice/Assets/Scripts/BossAI/Minibosses/Miniboss 1/MinibossTeleportingSkeletonAI.cs
+++ Wizard Apprentice/Assets/Scripts/BossAI/Minibosses/Miniboss 1/MinibossTeleportingSkeletonAI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Miniboss1AI : MonoBehaviour
+public class MinibossTeleportingSkeletonAI : MonoBehaviour
 {
 
 
