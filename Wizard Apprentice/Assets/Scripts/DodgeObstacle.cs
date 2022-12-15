a769166ce723ca91527c64ce3ecf26d6c7bd3724
sandeeen <60133440+sandeeen@users.noreply.github.com>
2022-12-15 15:51:25 +0100

    Event2

--- Wizard Apprentice/Assets/Scripts/DodgeObstacle.cs
+++ Wizard Apprentice/Assets/Scripts/DodgeObstacle.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class DodgeObstacle : MonoBehaviour
 {
 
     GameObject playerTarget;
     PlayerMovement playerMovement;
     [SerializeField] float TimesFailedDodgeroll = 0;
-
+    float counter;
 
     private void Start()
     {
         playerTarget = GameObject.FindGameObjectWithTag("Player");
         playerMovement = playerTarget.GetComponent<PlayerMovement>();
+        TimesFailedDodgeroll = 0;
+        gameObject.GetComponentInChildren<TextMeshPro>().enabled = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -24,16 +27,51 @@ public class DodgeObstacle : MonoBehaviour
             if (playerMovement.isDashing == false)
             {
             StartCoroutine(DeathByLava());
+                UpdateText();
             }
             
         }
     }
 
+    private void UpdateText()
+    {
+         
+        if (TimesFailedDodgeroll >= 3)
+        {
+            gameObject.GetComponentInChildren<TextMeshPro>().enabled = true;
+
+        }
+
+        if (TimesFailedDodgeroll >= 4 && counter < TimesFailedDodgeroll)
+        {
+            if (counter == 0)
+                counter = TimesFailedDodgeroll + 1;
+            else
+                counter ++;
+
+            gameObject.GetComponentInChildren<TextMeshPro>().text += "!";
+            gameObject.GetComponentInChildren<TextMeshPro>().fontSize += TimesFailedDodgeroll / 50;
+
+            if (TimesFailedDodgeroll >= 10)
+            {
+                gameObject.GetComponentInChildren<TextMeshPro>().color = Color.red;
+            }
+        }
+    }
+
     IEnumerator DeathByLava()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Vector2 Pos = playerTarget.transform.position - gameObject.transform.position;
 
-       // playerTarget.transform.position = new Vector3(0, -27f, 0);
+        playerTarget.GetComponent<Health>().RemoveHealth(5);
+
+        TimesFailedDodgeroll++;
+        playerTarget.GetComponent<PlayerMovement>().SetCanMove(false);
+        playerTarget.GetComponent<Rigidbody2D>().velocity = new Vector2(0, Pos.normalized.y * 5);
+        yield return new WaitForSeconds(0.5f);
+        playerTarget.GetComponent<PlayerMovement>().SetCanMove(true);
+
+
         yield return null;
     }
 
---
+++ Wizard Apprentice/Assets/TutorialPuzzle.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TutorialPuzzle : MonoBehaviour
+{
+
+    [SerializeField] float timer;
+    [SerializeField] bool hasEntered = false;
+
+    NormalCard normalCard;
+    TrippleShotCard trippleShotCard;
+    CircleShotCard circleShotCard;
+    CardHandler cardHandler;
+
+    AudioClip puzzleSuccess;
+
+    void Start()
+    {
+        gameObject.GetComponent<CapsuleCollider2D>().enabled = true;
+        normalCard = new NormalCard();
+        trippleShotCard = new TrippleShotCard();
+        circleShotCard = new CircleShotCard();
+
+        cardHandler = GameObject.FindGameObjectWithTag("GameController").GetComponent<CardHandler>();
+    }
+
+    void Update()
+    {
+
+        if (hasEntered == true)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer >= 60 && timer < 120)
+        {
+            gameObject.GetComponentInChildren<TextMeshPro>().text = "You can rearrange the spellcards";
+            SoundManager.Instance.PlayAudio(puzzleSuccess);
+        }
+
+        if (timer >= 120 && timer < 180)
+        {
+            gameObject.GetComponentInChildren<TextMeshPro>().text = "Use Mouse 1 to rearrange the 4 cards as shown on the sign";
+            SoundManager.Instance.PlayAudio(puzzleSuccess);
+        }
+
+
+
+        if (cardHandler.CheckInSlot(normalCard, 0) && cardHandler.CheckInSlot(trippleShotCard, 1) && cardHandler.CheckInSlot(circleShotCard, 2) && cardHandler.CheckInSlot(normalCard, 3))
+        {
+            Debug.Log("1");
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+
+            SoundManager.Instance.PlayAudio(puzzleSuccess);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision2D)
+    {
+        if (collision2D.CompareTag("Player"))
+        {
+            hasEntered = true;
+        }
+    }
+}
