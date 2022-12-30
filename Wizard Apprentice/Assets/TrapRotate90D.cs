7685d22b436853c73b5a82515fae69b37afc1f93
sandeeen <60133440+sandeeen@users.noreply.github.com>
2022-12-30 09:41:36 +0100

    Rotate90D

---
+++ Wizard Apprentice/Assets/TrapRotate90D.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapRotate90D : MonoBehaviour
+{
+    BulletHandler bulletHandler;
+    float timer;
+
+    [SerializeField] float damage = 10;
+    [SerializeField] float attackCoolDown = 2.5f;
+    [SerializeField] float bulletSpeed = 7.5f;
+    [SerializeField] float bulletSize = 1f;
+    [SerializeField] float shootingAmount = 1;
+    [SerializeField] int bulletAmount = 10;
+    [SerializeField] float angleChange = 3;
+    [SerializeField] float timeBetweenShots = 0.2f;
+
+    float aoeAngle = 0;
+    bool isShooting;
+
+
+    void Start()
+    {
+        isShooting = false;
+        bulletHandler = GameObject.FindGameObjectWithTag("GameController").GetComponent<BulletHandler>();
+        timer = 0;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (isShooting == false)
+        {
+            isShooting = true;
+            StartCoroutine(BasicAttack());
+            timer -= attackCoolDown;
+        }
+    }
+
+    IEnumerator BasicAttack()
+    {
+
+        for (int i = 0; i < shootingAmount; i++)
+        {
+            aoeAngle += angleChange;
+            bulletHandler.GetCircleShot(bulletAmount, gameObject, false, aoeAngle, damage, bulletSize, bulletSpeed);
+            yield return new WaitForSeconds(timeBetweenShots);
+
+        }
+        isShooting = false;
+        aoeAngle = 0;
+        yield return null;
+    }
+}
