4ca2e4c1bf132260c89a01de42af69c67eadb7f7
sandeeen <60133440+sandeeen@users.noreply.github.com>
2023-01-04 15:25:50 +0100

    Lightning strike card (CUM)

--- Wizard Apprentice/Assets/Scripts/Cards/LightningstrikeImpact.cs
+++ Wizard Apprentice/Assets/Scripts/Cards/LightningstrikeImpact.cs
@@ -18,18 +18,21 @@ public class LightningstrikeImpact : MonoBehaviour
     {
         playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
         enemyHealth = new List<Health>();
+        Camera.main.GetComponent<CameraMovement>().GetScreenShake(0.25f, 1);
 
         Destroy(gameObject, lifeTime);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.CompareTag("Enemy"))
         {
             collision.gameObject.GetComponent<Health>().RemoveHealth(playerStats.GetDamage(damage));
 
-           
         }
+        
+           
+        
     }
 
 
