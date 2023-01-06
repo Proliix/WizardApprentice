8405c561db3664d324df2a0956391558ca461840
Proliix <lucas.andreasson@hotmail.com>
2023-01-07 00:32:36 +0100

    buffed gravity shot

--- Wizard Apprentice/Assets/Scripts/Enemy AI/BatAI.cs
+++ Wizard Apprentice/Assets/Scripts/Enemy AI/BatAI.cs
@@ -9,13 +9,14 @@ public class BatAI : MonoBehaviour, IStunnable
     [SerializeField] float timer;
     [SerializeField] bool stunned = false;
 
-
+    Rigidbody2D rb2d;
     EnemyManager enemyManager;
 
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
         enemyManager = GameObject.FindWithTag("GameController").GetComponent<EnemyManager>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -31,8 +32,12 @@ public class BatAI : MonoBehaviour, IStunnable
 
     void MoveEnemy()
     {
-        float step = moveSpeed * Time.deltaTime;
-        transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+        //float step = moveSpeed * Time.deltaTime;
+        //transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+
+        Vector3 dir = (target.transform.position - transform.position).normalized;
+        rb2d.velocity = dir * moveSpeed;
+
     }
 
     public void GetStunned(float stunDuration = 0.25F)
